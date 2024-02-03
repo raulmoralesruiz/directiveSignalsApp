466a9e2f2186136b0030f46e2c2f9152db0fab70
Raúl Morales <raulmdaw@gmail.com>
2024-02-03 20:47:05 +0100

    329. Señales - Información del usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersService = inject(UsersService);
+  public userId = signal(1);
 
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() );
+    this.currentUser.set(undefined);
+  }
+
+  loadUser(id: number) {
+    if ( id <= 0) return;
+
+    this.userId.set(id);
+    this.usersService.getUserById(id).subscribe((user) => {
+      this.currentUser.set( user );
+    });
+  }
 }
