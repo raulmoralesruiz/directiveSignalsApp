48919c80d5a4ee326ed05a78ade790eafe51b358
Raúl Morales <raulmdaw@gmail.com>
2024-02-03 21:00:55 +0100

    330. Manejo de errores y propiedades computadas

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -13,17 +13,30 @@ export class UserInfoPageComponent implements OnInit {
   public currentUser = signal<User|undefined>(undefined);
   public userWasFound = signal(true);
 
+  public fullName = computed<string>( () => {
+    if ( !this.currentUser() ) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`
+  });
+
   ngOnInit(): void {
     this.loadUser( this.userId() );
-    this.currentUser.set(undefined);
   }
 
   loadUser(id: number) {
     if ( id <= 0) return;
 
     this.userId.set(id);
-    this.usersService.getUserById(id).subscribe((user) => {
-      this.currentUser.set( user );
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+        this.currentUser.set(undefined);
+      }
     });
   }
 }
